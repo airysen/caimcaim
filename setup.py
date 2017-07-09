a4de838decd933c8a66952e6992f481fc0e82aab
Arseniy Kustov <me@airysen.co>
2017-07-09 19:57:07 +0800

    Prepare for PyPi.

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(name='caimcaim',
-      version='0.1',
+      version='0.3',
       description='CAIM',
       long_description='CAIM Discretization Algorithm',
       classifiers=[
-          'Development Status :: Alpha',
-          'License :: MIT License',
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 3.6',
-          'Topic :: Supervised discretization',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Science/Research',
       ],
-      url='https://github.com/airysen/noisePGA',
+      url='https://github.com/airysen/caimcaim',
       author='Arseniy Kustov',
       author_email='me@airysen.co',
       license='MIT',
